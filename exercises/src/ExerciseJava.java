98cc361331186773d378e3a308c495bdb72ca8a9
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-02-29 13:37:35 -0600

    light exercise

--- exercises/src/ExerciseJava.java
+++ exercises/src/ExerciseJava.java
@@ -3,9 +3,15 @@ import java.util.Arrays;
 public class ExerciseJava {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(arrayOfMultiples(7, 5)));
-        System.out.println(Arrays.toString(arrayOfMultiples(12, 10)));
-        System.out.println(Arrays.toString(arrayOfMultiples(17, 6)));
+//        System.out.println(Arrays.toString(arrayOfMultiples(7, 5)));
+//        System.out.println(Arrays.toString(arrayOfMultiples(12, 10)));
+//        System.out.println(Arrays.toString(arrayOfMultiples(17, 6)));
+
+
+        System.out.println(Arrays.deepToString(squarePatch(3)));
+        System.out.println(Arrays.deepToString(squarePatch(5)));
+        System.out.println(Arrays.deepToString(squarePatch(1)));
+        System.out.println(Arrays.deepToString(squarePatch(0)));
 
     }
 
@@ -14,11 +20,32 @@ public class ExerciseJava {
 //  Problem: Array of Multiples
 //  task: Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num up to length.
 //  site: https://edabit.com/challenge/rzpucPyoyEtXPo2BG
-    public static int[] arrayOfMultiples(int num,int length){
+    private static int[] arrayOfMultiples(int num, int length){
         int[] newArray = new int[length];
         for (int i = 0; i<length;i++){
             newArray[i] = num*(i+1);
         }
         return newArray;
+    }
+
+    //  Problem: Perfect Square Patch
+    //  task: Create a function that takes an integer and outputs an n x n square solely consisting of the integer n.
+    //  site: https://edabit.com/challenge/7Tb7qMDQHtz3xpydd
+    private static int[][] squarePatch(int num){
+        int[][] square = new int[num][num];
+        if (num == 0){
+            return square;
+        }
+        for (int i = 0; i< num; i++){
+            for (int j =0; j<num; j++){
+                square[i][j] = num;
+            }
+        }
+//        for (int[] line: square) {
+//            for (int col: line) {
+//                col = num;
+//            }
+//        }
+        return square;
     }
 }
