6561857fb3f2cee85e76d432acc95583336c29f5
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-03-15 14:36:17 -0500

    js setup

--- exercises/src/javascript/jsExercise.js
+++ exercises/src/javascript/jsExercise.js
@@ -104,3 +104,13 @@ function oddishOrEvenish(num) {
         return "Oddish";
     }
 }
+
+//site https://edabit.com/challenge/94RHwrv6PfaqEGYtr
+//Calculate an Earned Run Average
+//Create a function that returns an Earned Run Average (ERA). An ERA is calculated by multiplying 9 by the quotient of Earned Runs Allowed er divided by ip Innings Pitched.
+// 
+// In baseball statistics, innings are represented with a fractional part of .1 (1/3) or .2 (2/3) to represent the number of outs in an inning. A whole number or a number with a fractional part of .0 represents a full inning with three outs. Check the Resources tab for a deeper explanation.
+//
+function era(num1,num2) {
+    
+}
